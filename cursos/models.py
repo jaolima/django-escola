8a29cf8c3e417296a34539651cfb15ae7f7e3da8
jaolima <joaolimafonseca2@gmail.com>
2022-10-09 16:00:21 -0300

    creating models from cursos app

--- cursos/models.py
+++ cursos/models.py
@@ -21,3 +21,20 @@ class Curso(Base):
 
         def __str__(self):
             return self.titulo
+
+class Avaliacao(Base):
+    curso = models.ForeignKey(Curso, related_name='avaliacoes', on_delete=models.CASCADE)
+    nome = models.CharField(max_length=255)
+    email = models.EmailField()
+    comentario = models.TextField(blank=True, default='')
+    avaliacao = models.DecimalField(max_digits=2, decimal_places=1)
+
+    class Meta:
+        verbose_name = 'Avaliacao'
+        verbose_name_plural = 'Avaliacoes'
+        unique_together = ['email', 'curso']
+
+        def __str__(self):
+            return f'{self.nome} avaliou o curso {self.curso} com nota {self.avaliacao}'
+
+    
