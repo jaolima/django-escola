93771d2386804bd111119d4f2958fa8bee6e8d7c
jaolima <joaolimafonseca2@gmail.com>
2022-10-09 15:54:27 -0300

    initial commit

---
+++ cursos/models.py
@@ -0,0 +1,23 @@
+from enum import auto
+from re import L
+from tabnanny import verbose
+from django.db import models
+
+class Base(models.Model):
+    criacao = models.DateTimeField(auto_now_add=True)
+    atualizacao = models.DateTimeField(auto_now=True)
+    ativo = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+class Curso(Base):
+    titulo = models.CharField(max_length=255)
+    url = models.URLField(unique=True)
+
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+
+        def __str__(self):
+            return self.titulo
