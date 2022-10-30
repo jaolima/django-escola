4872c06a13e0586574f36c06debae442081ae8a4
João Lima <joaolimafonseca2@gmail.com>
2022-10-30 00:39:06 -0300

    criando views

--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+# APIVIEW recebe a requisição
+from rest_framework.views import APIView
+# serve para retornar a responsta da requisição
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
+
+
+class CursoAPIView(APIView):
+    """
+    API de cursos da Geek
+    """
+
+    def get(self, request):
+        cursos = Curso.object.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+
+
+class AvaliacaoAPIView(APIView):
+    """
+    API de cursos da Geek
+    """
+
+    def get(self, request):
+        avaliacoes = Avaliacao.object.all()
+        serializer = AvaliacaoSerializer(avaliacoes, many=True)
+        return Response(serializer.data)
