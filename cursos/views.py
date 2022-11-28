e019f92e218dca62eb19242be53c85656e85969d
SuzanySouza <116468017+SuzanySouza@users.noreply.github.com>
2022-11-27 23:49:42 -0300

    implementing class based views ALL METHODS

--- cursos/views.py
+++ cursos/views.py
@@ -3,10 +3,10 @@ from rest_framework import generics
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
-class CursoAPIView(generics.ListCreateAPIView):
+class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
-class AvaliacaoAPIView(generics.ListCreateAPIView):
+class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
