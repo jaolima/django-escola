4e1c9fbdc3433b80080d666556941361198fb399
SuzanySouza <116468017+SuzanySouza@users.noreply.github.com>
2022-11-27 23:59:14 -0300

    implementing class based views ALL METHODS

--- cursos/urls.py
+++ cursos/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import CursoAPIView, AvaliacaoAPIView
+from .views import CursoAPIView,AvaliacoesAPIView, CursosAPIView,  AvaliacaoAPIView
 
 urlpatterns = [
-    path('cursos/', CursoAPIView.as_view(), name='cursos'),
-    path('avaliacoes/', AvaliacaoAPIView.as_view(), name='avaliacoes'),
+    path('cursos/', CursosAPIView.as_view(), name='cursos'),
+    path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
     path('cursos/<int:pk>/', CursoAPIView.as_view(), name='curso'),
     path('avaliacoes/<int:pk>/', AvaliacaoAPIView.as_view(), name='avaliacao'),
 ]
