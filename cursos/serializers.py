fa1b0af8494e5f3fa5cc035b38886cf26ce68b26
SuzanySouza <116468017+SuzanySouza@users.noreply.github.com>
2022-11-30 16:20:40 -0300

    creating nested data for serializer

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -23,6 +23,19 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
 
 
 class CursoSerializer(serializers.ModelSerializer):
+    # Nested Relationship
+    # avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
+
+    # Hyperlinked Related Field
+    # avaliacoes = serializers.HyperlinkedRelatedField(
+    #     many=True,
+    #     read_only=True,
+    #     view_name='avaliacao-detail'
+    # )
+
+    # Primary Key Related Field
+    avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
 
     class Meta:
         model = Curso
@@ -31,5 +44,6 @@ class CursoSerializer(serializers.ModelSerializer):
             'titulo',
             'url',
             'criacao',
-            'ativo'
+            'ativo',
+            'avaliacoes'
         )
