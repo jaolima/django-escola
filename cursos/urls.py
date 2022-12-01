42fb61f3d3d83de7cd8d112e360ab5ead5435dc6
joao.lima <joaolimafonseca2@gmail.com>
2022-12-01 12:54:49 -0300

    Using Simple Router

--- cursos/urls.py
+++ cursos/urls.py
@@ -1,10 +1,30 @@
 from django.urls import path
 
-from .views import CursoAPIView,AvaliacoesAPIView, CursosAPIView,  AvaliacaoAPIView
+from rest_framework.routers import SimpleRouter
+
+from .views import (
+    CursoAPIView,
+    AvaliacoesAPIView,
+    CursosAPIView,
+    AvaliacaoAPIView,
+    CursoViewSet,
+    AvaliacaoViewSet)
+
+
+router = SimpleRouter()
+router.register('cursos', CursoViewSet)
+router.register('avaliacoes', AvaliacaoViewSet)
+
 
 urlpatterns = [
     path('cursos/', CursosAPIView.as_view(), name='cursos'),
-    path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
     path('cursos/<int:pk>/', CursoAPIView.as_view(), name='curso'),
-    path('avaliacoes/<int:pk>/', AvaliacaoAPIView.as_view(), name='avaliacao'),
+    # Pegar todas as avaliações de um determinado curso
+    path('cursos/<int:curso_pk>/avaliacoes/', AvaliacoesAPIView.as_view(), name='cursos_avaliacoes'),
+    # Pegar determinada avaliaçõe de determinado curso
+    path('cursos/<int:curso_pk>/avaliacoes/<int:avaliacao_pk>/', AvaliacaoAPIView.as_view(), name='cursos_avaliacao'),
+
+    path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
+    path('avaliacoes/<int:avaliacao_pk>/', AvaliacaoAPIView.as_view(), name='avaliacao'),
 ]
+
